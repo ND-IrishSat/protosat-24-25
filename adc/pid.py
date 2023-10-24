5a3f0efb0babd95854ac7ab85936ece446e714f2
cmkuczun <93489352+cmkuczun@users.noreply.github.com>
2023-10-24 19:13:45 -0400

    Create pid.py

---
+++ adc/pid.py
@@ -0,0 +1,3 @@
+# TODO: this file will contain all code needed to run the PID process 
+# input = target state (either constant or equation)
+# output = changes that will be applied to actuators (reaction wheels, magnetorquers)
