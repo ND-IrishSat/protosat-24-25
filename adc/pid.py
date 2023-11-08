3d6fc94e3aa0963f54f1a0b01459daf024f8de7c
Henry Lemersal <lemersalhenry@gmail.com>
2023-11-07 19:43:27 -0500

    henry first commit

--- adc/pid.py
+++ adc/pid.py
@@ -1,3 +1,19 @@
 # TODO: this file will contain all code needed to run the PID process 
 # input = target state (either constant or equation)
 # output = changes that will be applied to actuators (reaction wheels, magnetorquers)
+
+from simple_pid import PID
+import time
+pid = PID(1, 0.1, 0.5, setpoint=9)
+
+# Assume we have a system we want to control in controlled_system
+
+while True:
+    # Compute new output from the PID according to the systems current value
+    control = pid(8)
+    print(control)
+    time.sleep(.05)
+    
+    
+    # Feed the PID output to the system and get its current value
+   
