795244dee716de923180f8a780dd21e650115f18
Henry Lemersal <lemersalhenry@gmail.com>
2023-11-28 18:56:18 -0500

    got example file

--- adc/pid.py
+++ adc/pid.py
@@ -4,15 +4,17 @@
 
 from simple_pid import PID
 import time
-pid = PID(1, 0.1, 0.5, setpoint=9)
+pid = PID(1, 0.1, 0.5, setpoint=9, sample_time=.01)
 
 # Assume we have a system we want to control in controlled_system
 
 while True:
     # Compute new output from the PID according to the systems current value
-    control = pid(8)
+    control = pid(v)
+
+
     print(control)
-    time.sleep(.05)
+    
     
     
     # Feed the PID output to the system and get its current value
