29ff80c69a1b082bc3ebe2d7ca14427b8ba3b4de
Rawan <rshehay2@nd.edu>
2024-10-10 18:09:37 -0400

    more comments

--- interface/vn100/vn100_interface.py
+++ interface/vn100/vn100_interface.py
@@ -7,16 +7,26 @@ test script for connecting to and reading data from vn100 imu
 '''
 from vnpy import *
 
-s = VnSensor()
+s = VnSensor() 
 
-def get_intance():
+def get_intance(): #returns the singleton instance of the VnSensor 
 	return s
 
 def connect():
+	'''
+	  connect(): connecting to the VnSensor version using specified COM port and printing connected to show 
+	'''
 	s.connect('COM5', 115200)
 	print("CONNECTED")
 
-def read_quat():
+def read_quat():#returnes a 1 by 4 array (w, x, y, z) of quaternion magnetic_acceleration_and_angular_rates
+	'''
+	readquat(): represnting the orientation of the object in 3d space through providing data about magentic acceleration and angular rates.
+
+	@return 
+	it returns a 1 by 4 array (w,x,y,z). w is the scalar part and x, y, z returns to vector part 
+	'''
+	
 	quat = s.read_quaternion_magnetic_acceleration_and_angular_rates().quat
 	quat = [quat.w, quat.x, quat.y, quat.z]
 	return quat
